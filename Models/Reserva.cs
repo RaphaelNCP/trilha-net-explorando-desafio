07aac0aee83da71d108fe14c65750a536df1a6d7
Raphael Nascimento <raphael.ofc11@gmail.com>
2024-01-30 12:55:31 -0300

    vaiiii

--- Models/Reserva.cs
+++ Models/Reserva.cs
@@ -15,16 +15,14 @@ namespace DesafioProjetoHospedagem.Models
 
         public void CadastrarHospedes(List<Pessoa> hospedes)
         {
-            // TODO: Verificar se a capacidade é maior ou igual ao número de hóspedes sendo recebido
-            // *IMPLEMENTE AQUI*
-            if (true)
+            bool eAlocavel = Suite.Capacidade == hospedes.Count 
+            if (eAlocavel)
             {
                 Hospedes = hospedes;
             }
             else
             {
-                // TODO: Retornar uma exception caso a capacidade seja menor que o número de hóspedes recebido
-                // *IMPLEMENTE AQUI*
+                throw new Exception("Capacidade menor que o número de hóspedes!");
             }
         }
 
@@ -35,23 +33,18 @@ namespace DesafioProjetoHospedagem.Models
 
         public int ObterQuantidadeHospedes()
         {
-            // TODO: Retorna a quantidade de hóspedes (propriedade Hospedes)
-            // *IMPLEMENTE AQUI*
-            return 0;
+            int quantidadeDeHospedes = Hospedes.Count;
+            return quantidadeDeHospedes;
         }
 
         public decimal CalcularValorDiaria()
         {
-            // TODO: Retorna o valor da diária
-            // Cálculo: DiasReservados X Suite.ValorDiaria
-            // *IMPLEMENTE AQUI*
-            decimal valor = 0;
-
-            // Regra: Caso os dias reservados forem maior ou igual a 10, conceder um desconto de 10%
-            // *IMPLEMENTE AQUI*
-            if (true)
+            decimal valorDiaria = DiasReservados * Suite.ValorDiaria;
+            decimal valor = valorDiaria:c;
+            bool eMaiorOuIgualADez = DiasReservados >= 10;
+            if (eMaiorOuIgualADez)
             {
-                valor = 0;
+                valor = valor - (valor * 0.10);
             }
 
             return valor;
